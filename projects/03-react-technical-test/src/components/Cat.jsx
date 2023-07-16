694ee272392bb7da610010ae4d25a5c929fecb17
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-16 10:08:30 +1000

    refactor: :recycle: refactor code with hooks preview move

--- projects/03-react-technical-test/src/components/Cat.jsx
+++ projects/03-react-technical-test/src/components/Cat.jsx
@@ -1,47 +1,30 @@
-import { useEffect, useState } from "react"
-import { CAT_PREFIX_IMAGE, CAT_RANDOM_IMAGE, CAT_RANDOM_WORD } from "../const/const"
+import { useCat } from "../hooks/useCat"
+import { useCatPhrase } from "../hooks/useCatPhrase"
 
 import "./Cat.css"
 
 export const Cat = () => {
-  const [phrase, setPhrase] = useState('No word')
-  const [cat, setCat] = useState({ id: null, url: '' })
+  const { phrase, refreshPhrase } = useCatPhrase()
+  const { id, url } = useCat({ phrase })
 
-  useEffect(() => {
-    async function getRandomFact() {
-      const respon = await fetch(CAT_RANDOM_WORD)
-      const dataJSON = await respon.json()
-      const { fact } = dataJSON
 
-      setPhrase(fact)
-
-      const threeFirstWords = fact.split(' ', 3).join(' ')
-
-      fetch(`${CAT_RANDOM_IMAGE}${threeFirstWords}?size=50&color=red&json=true`)
-        .then(respon => respon.json())
-        .then(data => {
-          const { _id, url } = data
-          const newCat = {
-            id: _id,
-            url: url
-          }
-
-          setCat(newCat)
-        })
-    }
-
-    getRandomFact()
-  }, [])
+  const handleClick = async () => {
+    refreshPhrase()
+  }
 
   return (
-    <article className="jd-cat" data-cat={cat.id}>
+    <article className="jd-cat" data-cat={id}>
       <header className="jd-cat--head">
         <p>{phrase}</p>
       </header>
 
       <picture className="jd-cat--picture">
-        <img src={`${CAT_PREFIX_IMAGE}${cat.url}`} alt={`Image extracted using the firts three words for ${phrase}`} />
+        <img src={url} alt={`Image extracted using the firts three words for ${phrase}`} />
       </picture>
+
+      <div className="jd-cat--actions">
+        <button className="btn-actions--new" onClick={handleClick}>New phrase cat</button>
+      </div>
     </article>
   )
 }
