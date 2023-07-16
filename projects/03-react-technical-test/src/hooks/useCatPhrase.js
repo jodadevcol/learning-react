14fba01cfcf395a74644450bd10f28316a3a07fc
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-16 10:07:29 +1000

    refactor: :recycle: refactor code and extract hook useCatPhrase

---
+++ projects/03-react-technical-test/src/hooks/useCatPhrase.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { getRandomFact } from "../services/phrase";
+
+export function useCatPhrase() {
+  const [phrase, setPhrase] = useState();
+
+  const refreshPhrase = () => {
+    getRandomFact().then((newFact) => setPhrase(newFact));
+  };
+
+  useEffect(refreshPhrase, []);
+
+  return { phrase, refreshPhrase };
+}
