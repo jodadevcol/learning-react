1639f348c6af92cf2c3b6372bd48c5b6a250fc96
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-17 08:20:39 +1000

    feat: :test_tube: add library for test in app

--- projects/03-react-technical-test/src/const/const.js
+++ projects/03-react-technical-test/src/const/const.js
@@ -1,7 +1,9 @@
 const { VITE_CAT_ENDPOINT_PHRASE_RA } = import.meta.env;
 const { VITE_CAT_ENDPOINT_IMAGE_URL } = import.meta.env;
 const { VITE_CAT_PREFIX_IMAGE } = import.meta.env;
+const { VITE_URL_LOCAL_PR } = import.meta.env;
 
 export const CAT_RANDOM_WORD = VITE_CAT_ENDPOINT_PHRASE_RA;
 export const CAT_RANDOM_IMAGE = VITE_CAT_ENDPOINT_IMAGE_URL;
 export const CAT_PREFIX_IMAGE = VITE_CAT_PREFIX_IMAGE;
+export const URL_LOCAL = VITE_URL_LOCAL_PR;
---
+++ projects/03-react-technical-test/tests/example.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { expect, test } from "@playwright/test";
+// import { URL_LOCAL } from "../src/const/const";
+
+const URL_LOCAL = "http://localhost:5173/";
+
+test("app show random phrase and image of cats", async ({ page }) => {
+  await page.goto(URL_LOCAL);
+
+  await expect(page).toHaveTitle(/Gatitos/);
+});
