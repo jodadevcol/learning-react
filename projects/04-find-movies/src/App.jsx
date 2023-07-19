26bc7ec43a1261cb0dc9d9d6e7416f2f3f7b0367
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-07-19 13:33:25 +1000

    refactor: :recycle: updated layout and structure of search movies in app

--- projects/04-find-movies/src/App.jsx
+++ projects/04-find-movies/src/App.jsx
@@ -1,60 +1,72 @@
+import { useCallback, useState } from 'react'
+import debounce from 'just-debounce-it';
+
+import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+import { useQuery } from './hooks/useQuery'
+
 import './App.css'
 
-import responMovies from './mocks/result-movies.json'
-// import responNoMovies from './mocks/result-no-movies.json'
-
 function App() {
-  const movies = responMovies.Search
-  const hasMovies = movies?.length > 0
+  const [sort, setSort] = useState(false)
+  const { query, setQuery, error } = useQuery()
+  const { movies, getMovies } = useMovies({ query, sort })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounceGetMovies = useCallback(
+    debounce(query => {
+      getMovies({ query })
+    }, 300)
+    , [getMovies]
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ query })
+
+    // FORMULARIOS NO CONTROLADOS (NO CONTROLADOS POR REACT)
+    // const fields = Object.fromEntries(new window.FormData(event.target))
+    // const { searchMovie } = fields
+
+    // console.log(searchMovie);
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return
+
+    setQuery(newQuery)
+    debounceGetMovies(newQuery)
+  }
+
+  const handleSorted = () => { setSort(!sort) }
 
   return (
     <>
       <header className='jd-head'>
         <h1 className='jd-head--title'>Movies</h1>
 
-        <form className='jd-head--form'>
-          <label htmlFor='search-movie'>
+        <form className='jd-head--form' onSubmit={handleSubmit}>
+          <label htmlFor='searchMovie'>
             {/* <span>Name movie</span> */}
-            <input type='text' id='search-movie' placeholder='Iron man, Matrix,Star Wars... ' />
+            <input type='text' name='searchMovie' id='searchMovie' placeholder='Iron man, Matrix,Star Wars... ' value={query} onChange={handleChange} />
+          </label>
+
+          <label htmlFor='sorted'>
+            {/* <span>Name movie</span> */}
+            <input type='checkbox' name='sorted' id='sorted' checked={sort} onChange={handleSorted} />
           </label>
 
           <button type='submit'><span>Find</span></button>
+
+          {
+            error && <p>{error}</p>
+          }
         </form>
       </header>
 
       <main>
-        <section>
-          {
-            hasMovies
-              ? (
-                <ul>
-                  {
-                    movies.map(movie => (
-                      <li key={movie.imdbID}>
-                        <article>
-                          <picture>
-                            <img src={movie.Poster} alt={`Movie poster of ${movie.Title}`} />
-                          </picture>
-
-                          <div>
-                            <h2>{movie.Title}</h2>
-
-                            <div>
-                              <span>{movie.Type}</span>
-                              <span>{movie.Year}</span>
-                            </div>
-                          </div>
-                        </article>
-                      </li>
-                    ))
-                  }
-                </ul>
-              )
-              : (
-                <p>There aren´t movies</p>
-              )
-          }
-        </section>
+        <Movies movies={movies} />
       </main>
     </>
   )
