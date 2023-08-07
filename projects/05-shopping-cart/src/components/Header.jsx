48958dbb00be860f5a048633511b576c2d67558b
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-08-07 22:59:27 +1000

    refactor: :coffin: remove function prop in child components

--- projects/05-shopping-cart/src/components/Header.jsx
+++ projects/05-shopping-cart/src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Filters } from './Filters.jsx'
 
 import './Header.css'
 
-export function Header ({ onChange }) {
+export function Header () {
   return (
     <header className='shop-hero'>
       <h1 className='shop-hero--title'>Shopping Cart</h1>
 
-      <Filters onChange={onChange} />
+      <Filters />
     </header>
   )
 }
