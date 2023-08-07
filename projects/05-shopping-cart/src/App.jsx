9112d86b750ded23df18ecfc4aaed1c9e53b9dba
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-08-07 22:58:50 +1000

    fix: add provider of useContext in child components

--- projects/05-shopping-cart/src/App.jsx
+++ projects/05-shopping-cart/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Header } from './components/Header'
 import { Products } from './components/Products'
 import { Footer } from './components/Footer'
+import { Cart } from './components/Cart'
 
 import { useFilters } from './hooks/useFilters'
 
@@ -9,27 +10,29 @@ import { IS_DEVELOPMENT } from './config'
 import { products as InitialProducts } from './mockus/products.json'
 
 import './App.css'
-import { Cart } from './components/Cart'
+import { CartProvider } from './context/cart'
 
 function App () {
   const [products] = useState(InitialProducts)
-  const { filters, setFilters, filterProducts } = useFilters()
+  const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <Header onChange={setFilters} />
+      <CartProvider>
+        <Header />
 
-      <Cart />
+        <Cart />
 
-      <main id='content'>
-        <Products products={filteredProducts} />
-      </main>
+        <main id='content'>
+          <Products products={filteredProducts} />
+        </main>
 
-      {
-        IS_DEVELOPMENT && <Footer filters={filters} />
-      }
+        {
+          IS_DEVELOPMENT && <Footer />
+        }
+      </CartProvider>
     </>
   )
 }
