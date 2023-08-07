84f3ce5a05789c47e8530287bd68c8871b778cf2
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-08-07 22:55:48 +1000

    feat: add new component cart of ecommerce

--- projects/05-shopping-cart/src/components/Cart.jsx
+++ projects/05-shopping-cart/src/components/Cart.jsx
@@ -11,40 +11,47 @@ export function Cart () {
   }
 
   return (
-    <aside className='cart-shop'>
-      <div className={`cart-wrapper ${toggleCart ? 'opened' : 'closed'}`}>
-        <div className='cart-shopping'>
-          <button
-            className={`cart-shopping--btn cart-shopping--${toggleCart ? 'open' : 'close'}`}
-            onClick={handleClickCart}
-          >
-            <CartIcon />
-          </button>
-        </div>
+    <>
+      <aside className='cart-shopping'>
+        <button
+          className={`cart-shopping--btn cart-shopping--${toggleCart ? 'open' : 'close'}`}
+          onClick={handleClickCart}
+        >
+          <CartIcon />
+        </button>
+      </aside>
 
-        <div className={`cart-content ${toggleCart ? 'cart-open' : 'cart-close'}`}>
-          <article className='cart-content--wrap'>
-            <ul className='cart-content--list'>
-              <li className='cart-content--item'>
-                <div>
-                  <img src='https://i.dummyjson.com/data/products/6/thumbnail.png' alt='Imagen del Macbook Pro' />
+      <aside className={`cart-shop ${toggleCart ? 'visible' : 'hidden'}`}>
+        <div className={`cart-wrapper ${toggleCart ? 'opened' : 'closed'}`}>
+          <div className={`cart-content ${toggleCart ? 'cart-open' : 'cart-close'}`}>
+            <article className='cart-content--wrap'>
+              <ul className='cart-content--list'>
+                <li className='cart-content--item'>
+                  <div className='cart-item'>
+                    <div className='cart-item--left'>
+                      <img src='https://i.dummyjson.com/data/products/6/thumbnail.png' alt='Imagen del Macbook Pro' />
+                    </div>
 
-                  <div>
-                    <span>MacBook Pro - $ <strong>1.340</strong></span>
+                    <div className='cart-item--right'>
+                      <div className='cart-item-right--title'>
+                        <span>MacBook Pro - $ <strong>1.340</strong></span>
+                      </div>
+
+                      <div className='cart-item-right--qty'>
+                        <span>
+                          Qty: 1
+                        </span>
+                        <button>+</button>
+                      </div>
+                    </div>
                   </div>
-
-                  <footer>
-                    <span>
-                      Cantidad: 1
-                    </span>
-                    <button>+</button>
-                  </footer>
-                </div>
-              </li>
-            </ul>
-          </article>
+                </li>
+              </ul>
+            </article>
+          </div>
         </div>
-      </div>
-    </aside>
+      </aside>
+    </>
+
   )
 }
