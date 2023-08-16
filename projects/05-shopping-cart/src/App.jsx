93e131ba920e0d5c70ceb91f8be364ffcc34580c
Daniel Garcia (jodadev.col) <jodadev.col@gmail.com>
2023-08-16 21:58:05 +1000

    refactor: :iphone: updated new design of project

--- projects/05-shopping-cart/src/App.jsx
+++ projects/05-shopping-cart/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Header } from './components/Header'
 import { Products } from './components/Products'
 import { Footer } from './components/Footer'
-import { Cart } from './components/Cart'
 
 import { useFilters } from './hooks/useFilters'
 
@@ -11,6 +10,7 @@ import { products as InitialProducts } from './mockus/products.json'
 
 import './App.css'
 import { CartProvider } from './context/cart'
+import { Filters } from './components/Filters'
 
 function App () {
   const [products] = useState(InitialProducts)
@@ -23,10 +23,11 @@ function App () {
       <CartProvider>
         <Header />
 
-        <Cart />
-
         <main id='content'>
-          <Products products={filteredProducts} />
+          <div className='max-w-[90vw] mx-auto mt-16'>
+            <Filters />
+            <Products products={filteredProducts} />
+          </div>
         </main>
 
         {
--- projects/05-shopping-cart/src/components/Header.jsx
+++ projects/05-shopping-cart/src/components/Header.jsx
@@ -1,13 +1,22 @@
-import { Filters } from './Filters.jsx'
-
 import './Header.css'
+import { MainLogo, MenuIcon } from './Icons'
 
 export function Header () {
   return (
-    <header className='shop-hero'>
-      <h1 className='shop-hero--title'>Shopping Cart</h1>
+    <header className='fixed top-0 left-0 right-0 w-full max-w-full h-auto min-h-[60px] z-0'>
+      <div className='relative w-full max-w-6xl after:block after:absolute after:bottom-0 after:w-full after:h-1 after:bg-col-light-gray'>
+        <div className='flex items-center gap-x-4 py-5 px-6'>
+          <button className='w-full max-w-[16px] h-4 text-col-dark-gray'>
+            <MenuIcon />
+          </button>
 
-      <Filters />
+          <a className='w-full max-w-[136px] text-col-dark-blue' href='/'>
+            <MainLogo />
+          </a>
+
+          {/* <Cart /> */}
+        </div>
+      </div>
     </header>
   )
 }
