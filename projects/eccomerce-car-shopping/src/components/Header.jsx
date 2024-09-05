343bead0492e033bb5ad60b5ff2886c393297382
Danni Garcia <jodadev.col@gmail.com>
2024-09-05 22:12:41 +1000

    feat: Add Filters component to the Header and context by filters

--- projects/eccomerce-car-shopping/src/components/Header.jsx
+++ projects/eccomerce-car-shopping/src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { Filters } from './Filters'
+
 export function Header() {
   return (
     <header>
       <h1 className='font-bold text-3xl text-white'>Shopping cart 🛒</h1>
 
-      <div className='mt-5'>Filters</div>
+      <Filters />
     </header>
   )
 }
---
+++ projects/eccomerce-car-shopping/src/context/FilterContext.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from 'react'
+
+export const FilterContext = createContext()
+
+export function FilterProvider({ children }) {
+  const [filters, setFilters] = useState({ category: 'all', minPrice: 0 })
+
+  return (
+    <FilterContext.Provider
+      value={{
+        filters,
+        setFilters
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  )
+}
