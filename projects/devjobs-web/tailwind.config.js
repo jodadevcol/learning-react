b72248407a6b43929592e2ce3b82080d9c20f47b
Danni Garcia <jodadev.col@gmail.com>
2024-09-08 16:38:10 +1000

    chore: Update tailwind.config.js with dark mode, new colors, and maxWidth

--- projects/devjobs-web/tailwind.config.js
+++ projects/devjobs-web/tailwind.config.js
@@ -3,10 +3,27 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'selector',
   theme: {
     extend: {
       fontFamily: {
         sans: ['Kumbh Sans Variable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          100: '#5964E0', // violet
+          200: '#939BF4', // light violet
+          300: '#19202D', // Very Dark Blue
+          400: '#121721', // Midnight
+        },
+        secondary: {
+          100: '#F4F6F8', // light gray
+          200: '#9DAEC2', // gray
+          300: '#6E8098', // dark gray
+        },
+      },
+      maxWidth: {
+        'screen-2xl': '1440px',
       },
     },
   },
