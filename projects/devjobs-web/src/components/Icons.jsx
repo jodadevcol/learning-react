4345341dc725e811acd8ec4833fb16e5a219a5b8
Danni Garcia <jodadev.col@gmail.com>
2024-09-09 19:21:16 +1000

    refactor: Add LocationIcon and SearchIcon components for improved search functionality

--- projects/devjobs-web/src/components/Icons.jsx
+++ projects/devjobs-web/src/components/Icons.jsx
@@ -19,4 +19,31 @@ function MoonIcon(props) {
   )
 }
 
-export { MoonIcon, SunIcon }
+function LocationIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0.09 0 16.72 23.89"
+      {...props}>
+      <path
+        d="M14.358 2.451A8.3 8.3 0 0 0 8.448 0a8.3 8.3 0 0 0-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 0 1-3.051-3.054 3.057 3.057 0 0 1 3.05-3.055 3.057 3.057 0 0 1 3.052 3.055 3.057 3.057 0 0 1-3.051 3.054z"
+        fill="currentColor"></path>
+    </svg>
+  )
+}
+
+function SearchIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 23.94 23.94"
+      {...props}>
+      <path
+        d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 0 0 2.15-5.784A8.898 8.898 0 0 0 8.898 0 8.898 8.898 0 0 0 0 8.898a8.898 8.898 0 0 0 8.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 1 1 8.9 2.737a6.16 6.16 0 0 1 0 12.322z"
+        fill="currentColor"></path>
+    </svg>
+  )
+}
+
+export { LocationIcon, MoonIcon, SearchIcon, SunIcon }
+
