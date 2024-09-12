be43508580cd6aa724d186facc5dcf5d2069eca7
Danni Garcia <jodadev.col@gmail.com>
2024-09-12 18:20:10 +1000

    refactor: Update JobFilter component styling and structure

--- projects/devjobs-web/src/components/jobs/JobDetails.jsx
+++ projects/devjobs-web/src/components/jobs/JobDetails.jsx
@@ -1,4 +1,157 @@
 /* eslint-disable react/prop-types */
+
+function JobDetailHeader({ logo, logoBackground, company }) {
+  return (
+    <header className="flex w-full items-center overflow-hidden rounded-md bg-white dark:bg-primary-300">
+      <div
+        className="flex aspect-square w-full max-w-36 items-center justify-center"
+        style={{ backgroundColor: logoBackground }}>
+        <img
+          className="block w-full max-w-24 object-contain"
+          src={`../${logo}`}
+          alt={company}
+        />
+      </div>
+
+      <div className="flex flex-grow items-center justify-between px-10 py-8">
+        <div>
+          <span className="text-heading-200 font-bold text-primary-300 dark:text-white">
+            {company}
+          </span>
+        </div>
+
+        <a
+          href=""
+          className="w-full max-w-fit rounded-md bg-primary-100/10 px-5 py-3 dark:bg-white/10">
+          <span className="whitespace-nowrap text-base font-bold capitalize text-primary-100 dark:text-white">
+            Company site
+          </span>
+        </a>
+      </div>
+    </header>
+  )
+}
+
+function JobDetailFooter({ position, company }) {
+  return (
+    <footer className="relative mt-12 bg-primary-300 before:absolute before:bottom-0 before:left-full before:top-0 before:h-full before:w-full before:bg-primary-300 after:absolute after:bottom-0 after:right-full after:top-0 after:h-full after:w-full after:bg-primary-300">
+      <div className="flex items-center justify-between gap-x-8 px-5 py-6">
+        <div className="text-primary-300 dark:text-white">
+          <h1 className="mb-3 text-heading-300 font-bold">{position}</h1>
+
+          <span className="text-base text-secondary-300">{company}</span>
+        </div>
+
+        <a
+          href="#"
+          className="w-full max-w-fit rounded-md bg-primary-100 px-5 py-3">
+          <span className="whitespace-nowrap text-base font-bold capitalize text-white">
+            Apply Now
+          </span>
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+function JobDetailRequirements({ id, requirements }) {
+  return (
+    <div>
+      <h2 className="mb-6 text-heading-300 font-bold text-primary-300 dark:text-white">
+        Requirements
+      </h2>
+
+      <div className="mb-6 text-left text-body-100 text-secondary-300 dark:text-secondary-200">
+        <p>{requirements?.content}</p>
+      </div>
+
+      <ul className="list-disc">
+        {requirements.items?.map((item, index) => {
+          return (
+            <li
+              key={index + id}
+              className="mb-2 inline-flex w-full max-w-full items-start justify-start gap-x-8 whitespace-normal text-body-100 text-secondary-300 last:mb-0 dark:text-secondary-200">
+              <span className="h-1 w-1 min-w-1 translate-y-2 rounded-full bg-primary-100 text-body-100"></span>
+              <div>
+                <p>{item}</p>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function JobDetailRole({ id, role }) {
+  return (
+    <div>
+      <h2 className="mb-6 text-heading-300 font-bold text-primary-300 dark:text-white">
+        What You Will Do
+      </h2>
+
+      <div className="mb-6 text-left text-body-100 text-secondary-300 dark:text-secondary-200">
+        <p>{role?.content}</p>
+      </div>
+
+      <ul className="list-disc">
+        {role.items?.map((item, index) => {
+          return (
+            <li
+              key={index + id}
+              className="mb-2 inline-flex w-full max-w-full items-start justify-start text-body-100 text-secondary-300 last:mb-0 dark:text-secondary-200">
+              <span className="mr-8 text-body-100 font-bold text-primary-100">
+                {index + 1}
+              </span>
+              {item}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function JobDetailInfo({
+  postedAt,
+  contract,
+  position,
+  location,
+  description,
+}) {
+  return (
+    <div className="flex flex-wrap items-center justify-between gap-y-11">
+      <div className="flex-grow">
+        <div className="mb-2 flex items-center justify-start gap-x-3 text-secondary-300 dark:text-secondary-200">
+          <span className="text-body-100 capitalize">{postedAt}</span>
+          <span>&#x2022;</span>
+          <span className="text-body-100 capitalize">{contract}</span>
+        </div>
+
+        <div className="text-primary-300 dark:text-white">
+          <h1 className="mb-3 text-heading-100 font-bold">{position}</h1>
+
+          <span className="text-heading-400 font-bold text-primary-100">
+            {location}
+          </span>
+        </div>
+      </div>
+
+      <a
+        href="#"
+        className="w-full max-w-fit rounded-md bg-primary-100 px-5 py-3 lg:px-10">
+        <span className="whitespace-nowrap text-base font-bold capitalize text-white">
+          Apply Now
+        </span>
+      </a>
+
+      <div className="flex-auto text-left text-body-100 text-secondary-300 dark:text-secondary-200">
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
 function JobDetail({ job }) {
   const {
     id,
@@ -18,115 +171,29 @@ function JobDetail({ job }) {
     <article
       className="relative mx-auto flex max-w-3xl flex-col gap-y-8"
       data-job={id}>
-      <header className="bg-primary-300 flex w-full items-center overflow-hidden rounded-md">
-        <div
-          className="flex aspect-square w-full max-w-36 items-center justify-center"
-          style={{ backgroundColor: logoBackground }}>
-          <img
-            className="block w-full max-w-24 object-contain"
-            src={`../${logo}`}
-            alt={company}
-          />
-        </div>
+      <JobDetailHeader
+        logo={logo}
+        logoBackground={logoBackground}
+        company={company}
+      />
 
-        <div className="flex flex-grow items-center justify-between px-10 py-8">
-          <div>
-            <span className="text-heading-200 font-bold dark:text-white">
-              {company}
-            </span>
-          </div>
+      <div className="flex flex-col gap-y-12 rounded-md bg-white p-12 dark:bg-primary-300">
+        <JobDetailInfo
+          position={position}
+          postedAt={postedAt}
+          contract={contract}
+          location={location}
+          description={description}
+        />
 
-          <a
-            href=""
-            className="w-full max-w-fit rounded-md bg-white/10 px-5 py-3">
-            <span className="whitespace-nowrap text-base font-bold capitalize text-white">
-              Company site
-            </span>
-          </a>
-        </div>
-      </header>
+        {requirements != null && (
+          <JobDetailRequirements id={id} requirements={requirements} />
+        )}
 
-      <div className="bg-primary-300 flex flex-col gap-y-12 rounded-md p-12">
-        <div className="flex flex-wrap items-center justify-between gap-y-11">
-          <div className="flex-grow">
-            <div className="text-secondary-300 mb-2 flex items-center justify-start gap-x-3">
-              <span className="text-body-100 capitalize">{postedAt}</span>
-              <span>&#x2022;</span>
-              <span className="text-body-100 capitalize">{contract}</span>
-            </div>
-
-            <div className="text-primary-300 dark:text-white">
-              <h1 className="text-heading-100 mb-3 font-bold">{position}</h1>
-
-              <span className="text-heading-400 text-primary-100 font-bold">
-                {location}
-              </span>
-            </div>
-          </div>
-
-          <a
-            href="#"
-            className="bg-primary-100 w-full max-w-fit rounded-md px-5 py-3">
-            <span className="whitespace-nowrap text-base font-bold capitalize text-white">
-              Apply Now
-            </span>
-          </a>
-
-          <div className="text-body-100 text-secondary-200 flex-auto text-left">
-            <p>{description}</p>
-          </div>
-        </div>
-
-        <div>
-          <h2 className="text-heading-300 mb-6 font-bold text-white">
-            Requirements
-          </h2>
-
-          <div className="text-body-100 text-secondary-200 mb-6 text-left">
-            <p>{requirements?.content}</p>
-          </div>
-
-          <ul className="list-disc">
-            {requirements.items?.map((item, index) => {
-              return (
-                <li
-                  key={index + id}
-                  className="text-body-100 text-secondary-200 mb-2 inline-flex w-full max-w-full items-center justify-start last:mb-0">
-                  <span className="bg-primary-100 mr-8 h-1 w-1 rounded-full"></span>
-                  {item}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-
-        <div>
-          <h2 className="text-heading-300 mb-6 font-bold text-white">
-            What You Will Do
-          </h2>
-
-          <div className="text-body-100 text-secondary-200 mb-6 text-left">
-            <p>{role?.content}</p>
-          </div>
-
-          <ul className="list-disc">
-            {role.items?.map((item, index) => {
-              return (
-                <li
-                  key={index + id}
-                  className="text-body-100 text-secondary-200 mb-2 inline-flex w-full max-w-full items-start justify-start last:mb-0">
-                  <span className="text-primary-100 text-body-100 mr-8 font-bold">
-                    {index + 1}
-                  </span>
-                  {item}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        {role != null && <JobDetailRole id={id} role={role} />}
       </div>
 
-      <footer></footer>
+      <JobDetailFooter position={position} company={company} />
     </article>
   )
 }
--- projects/devjobs-web/src/components/jobs/JobFilter.jsx
+++ projects/devjobs-web/src/components/jobs/JobFilter.jsx
@@ -6,9 +6,6 @@ function JobFilter() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-
-    // const newJobsFiltered = filteredJobs(jobs)
-    // console.log(newJobsFiltered)
   }
 
   const handleChangeFullTimeOnly = (event) => {
@@ -42,18 +39,18 @@ function JobFilter() {
   }
 
   return (
-    <div className="dark:bg-primary-300 rounded-md bg-white">
+    <div className="rounded-md bg-white dark:bg-primary-300">
       <form
         onSubmit={handleSubmit}
         className="relative flex h-20 max-h-20 items-center justify-between overflow-hidden">
-        <div className="flex-auto flex-grow px-8 py-4">
+        <div className="max-w-[500px] flex-auto flex-grow px-8 py-4">
           <div className="flex items-center justify-start gap-x-4">
-            <span className="text-primary-100 max-w-6">
+            <span className="max-w-6 text-primary-100">
               <SearchIcon className="h-full w-full max-w-full" />
             </span>
             <label htmlFor="search-by-propertys" className="w-full">
               <input
-                className="caret-primary-100 w-full appearance-none border-none bg-transparent text-base text-white outline-none placeholder:text-white/50"
+                className="w-full appearance-none border-none bg-transparent text-base text-primary-300 caret-primary-100 outline-none placeholder:text-primary-300 dark:text-white dark:placeholder:text-white/50"
                 type="text"
                 name="search-by-propertys"
                 id="search-by-propertys"
@@ -63,15 +60,15 @@ function JobFilter() {
             </label>
           </div>
         </div>
-        <div className="bg-secondary-300/20 h-full min-h-max w-[1px]"></div>
-        <div className="max-w-[300px] px-6 py-4">
+        <div className="h-full min-h-max w-[1px] bg-secondary-300/20"></div>
+        <div className="w-full max-w-[300px] px-6 py-4">
           <label htmlFor="search-by-location">
             <div className="flex items-center justify-start gap-x-4">
-              <span className="text-primary-100 max-w-5">
+              <span className="max-w-5 text-primary-100">
                 <LocationIcon className="h-full w-full max-w-full" />
               </span>
               <input
-                className="caret-primary-100 appearance-none border-none bg-transparent text-base text-white outline-none placeholder:text-white/50"
+                className="w-full appearance-none border-none bg-transparent text-base text-primary-300 caret-primary-100 outline-none placeholder:text-primary-300 dark:text-white dark:placeholder:text-white/50"
                 type="text"
                 name="search-by-location"
                 id="search-by-location"
@@ -81,7 +78,7 @@ function JobFilter() {
             </div>
           </label>
         </div>
-        <div className="bg-secondary-300/20 h-full min-h-max w-[1px]"></div>
+        <div className="h-full min-h-max w-[1px] bg-secondary-300/20"></div>
         <div className="flex max-w-[345px] items-center justify-end gap-x-6 px-6 py-4">
           <label
             htmlFor="full-time-only"
@@ -94,16 +91,16 @@ function JobFilter() {
                 onChange={handleChangeFullTimeOnly}
                 id="full-time-only"
               />
-              <span className="peer-checked/full-time:bg-primary-100 pointer-events-none absolute inset-0 bg-white/10 bg-center bg-no-repeat after:block after:h-full after:w-full after:bg-[url('./assets/icons/check.svg')] after:bg-[length:12px_10px] after:bg-center after:bg-no-repeat after:opacity-0 peer-checked/full-time:after:opacity-100"></span>
+              <span className="pointer-events-none absolute inset-0 bg-primary-300/10 bg-center bg-no-repeat after:block after:h-full after:w-full after:bg-[url('./assets/icons/check.svg')] after:bg-[length:12px_10px] after:bg-center after:bg-no-repeat after:opacity-0 peer-checked/full-time:bg-primary-100 peer-checked/full-time:after:opacity-100 dark:bg-white/10"></span>
             </div>
-            <span className="whitespace-nowrap text-base font-bold capitalize text-white">
+            <span className="darktext-white whitespace-nowrap text-base font-bold capitalize text-primary-300 dark:text-white">
               Full time only
             </span>
           </label>
 
           <button
             type="submit"
-            className="bg-primary-100 w-full max-w-[124px] rounded-md px-5 py-3 lg:px-10">
+            className="w-full max-w-[124px] rounded-md bg-primary-100 px-5 py-3 lg:px-10">
             <span className="text-base font-bold capitalize text-white">
               Search
             </span>
