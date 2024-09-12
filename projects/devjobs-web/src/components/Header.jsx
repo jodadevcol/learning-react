5fb40f80432beb8a2b639be2ee61ba133b0a3098
Danni Garcia <jodadev.col@gmail.com>
2024-09-12 18:20:15 +1000

    feat: Add lazy loading for images in Header component

--- projects/devjobs-web/src/components/Header.jsx
+++ projects/devjobs-web/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'wouter'
+import MaskHeader from './../assets/mask/mask-header.png'
 import MainLogo from './MainLogo'
 import ToggleTheme from './ToogleTheme'
 
@@ -5,15 +7,18 @@ function Header() {
   return (
     <header className="relative min-h-40">
       <div
-        className="bg-primary-100 absolute inset-0 -z-10 bg-left bg-repeat-x"
-        style={{ backgroundImage: 'url(./mask-header.png)' }}></div>
+        className="absolute inset-0 -z-10 bg-primary-100 bg-left bg-repeat-x"
+        style={{
+          backgroundImage: `url(${MaskHeader})`,
+          borderRadius: '25% 25% 25% 6% / 0% 0% 0% 50%',
+        }}></div>
 
       <div className="container-layout flex items-start justify-between pb-6 pt-12 text-white">
-        <a href="#" className="block w-full max-w-[116px] text-current">
+        <Link href="/" className="block w-full max-w-[116px] text-current">
           <h1 className="hidden">Devjobs</h1>
 
           <MainLogo />
-        </a>
+        </Link>
 
         <ToggleTheme />
       </div>
