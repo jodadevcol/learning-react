d91053e5194112ed6ad2c1faa5d9e4edb52878e9
Danni Garcia <jodadev.col@gmail.com>
2024-09-15 16:47:05 +1000

    Add sonner package for notifications of project

--- projects/05-crud-redux-react-ts/src/App.tsx
+++ projects/05-crud-redux-react-ts/src/App.tsx
@@ -2,13 +2,18 @@ import '@/App.css'
 import { Card } from '@tremor/react'
 import CreateNewUser from '@users/components/CreateNewUser'
 import ListUsers from '@users/components/ListUsers'
+import { Toaster } from 'sonner'
 
 function App() {
   return (
-    <Card className='flex items-start justify-between gap-x-5'>
-      <ListUsers />
-      <CreateNewUser />
-    </Card>
+    <>
+      <Card className='flex items-start justify-between gap-x-5'>
+        <ListUsers />
+        <CreateNewUser />
+      </Card>
+
+      <Toaster richColors />
+    </>
   )
 }
 
