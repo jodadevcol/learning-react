01b2562cc06a3f1d701024077b989e664f1b4ad4
Danni Garcia <jodadev.col@gmail.com>
2024-09-15 15:21:04 +1000

    Refactor project structure and add path aliases for better module imports

--- projects/05-crud-redux-react-ts/vite.config.ts
+++ projects/05-crud-redux-react-ts/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, './src/components')
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, './src/hooks')
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, './src/pages')
+      },
+      {
+        find: '@store',
+        replacement: path.resolve(__dirname, './src/store')
+      },
+      {
+        find: '@users',
+        replacement: path.resolve(__dirname, './src/users')
+      }
+    ]
+  }
 })
